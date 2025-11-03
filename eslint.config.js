c5d3cd013dada8526e35f980b797782c1cc640d6
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-11-03 23:48:25 +0300

    Отформатировала код

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,23 @@
 import js from "@eslint/js";
+import ts from "typescript-eslint";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-      eslintConfigPrettier,
-    ],
+    extends: [js.configs.recommended, ts.configs.recommended, reactRefresh.configs.vite],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs["recommended-latest"].rules,
+    },
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
     },
   },
